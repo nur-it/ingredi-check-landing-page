1320375fdba8b02f201a3720f2564198eed4dc35
devwithfarshi <devwithfarshi@gmail.com>
2024-04-21 13:45:04 +0600

    features section done

--- src/components/screen/Home/HomeFeatures.tsx
+++ src/components/screen/Home/HomeFeatures.tsx
@@ -1,19 +1,34 @@
-import { featuresData } from "../../../data/featuresData";
+import { featuresData, FeaturesDataType } from "../../../data/featuresData";
 
 const HomeFeatures = () => {
   return (
-    <section className="bg-[url(/bgs/featuresBg.svg)] bg-no-repeat bg-cover pt-[10rem]">
+    <section className="bg-[url(/bgs/featuresBg.svg)] bg-no-repeat bg-cover pt-[10rem] pb-[13.2rem] md:pb-[27rem]">
       <div className="container">
         <h2 className="font-fredoka font-semibold text-white text-center text-[4rem]">
           Features at Your Fingertips
         </h2>
-        <div className="flex flex-wrap justify-between gap-y-[2.4rem]">
-          {/* {featuresData.map((item) => {
-            return(
-                {item.title}
-            )
-          })} */}
-        </div>
+        <ul className="mt-[4rem] md:mt-[8rem] flex flex-wrap justify-between gap-y-[2.4rem]">
+          {featuresData.map((item: FeaturesDataType) => {
+            return (
+              <li
+                key={item.id}
+                className="bg-white p-[3.2rem] md:p-[5rem] rounded-[1.218rem] md:rounded-[2rem] md:w-[calc(100%_/_2_-_2.4rem)] h-[22rem] md:h-[36.2rem] flex flex-col justify-between"
+              >
+                <img
+                  src={item.icon}
+                  alt={item.title}
+                  className="w-[4rem] md:w-[8rem]"
+                />
+                <h3 className="font-bold text-dark-dark text-[1.6rem] md:text-[2.4rem]">
+                  {item.title}
+                </h3>
+                <p className="text-dark-dark2 text-[1.4rem] md:text-[1.6rem] leading-[1.8rem] md:leading-[3rem]">
+                  {item.desc}
+                </p>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </section>
   );
--- src/data/featuresData.ts
+++ src/data/featuresData.ts
@@ -2,27 +2,35 @@ import featuresIcon1 from "../assets/images/i/featuresIcon1.svg";
 import featuresIcon2 from "../assets/images/i/featuresIcon2.svg";
 import featuresIcon3 from "../assets/images/i/featuresIcon3.svg";
 import featuresIcon4 from "../assets/images/i/featuresIcon4.svg";
-export const featuresData = [
+
+export type FeaturesDataType = {
+  id: string;
+  title: string;
+  icon: string;
+  desc: string;
+};
+
+export const featuresData: FeaturesDataType[] = [
   {
-    id: 1,
+    id: "1re",
     title: "Personalized Dietary Preferences",
     icon: featuresIcon1,
     desc: "Enter your dietary needs in natural language, and let IngrediCheck do the rest.",
   },
   {
-    id: 2,
+    id: "2ue",
     title: "Instant Barcode Analysis",
     icon: featuresIcon2,
     desc: "Scan a packaged food item's barcode for an immediate thumbs-up or thumbs-down based on your dietary profile",
   },
   {
-    id: 3,
+    id: "3wer",
     title: "Picture Perfect",
     icon: featuresIcon3,
     desc: "If the barcode scan draws a blank, snap a picture. Our database will still provide an instant analysis.",
   },
   {
-    id: 4,
+    id: "4pr",
     title: "Favorites List",
     icon: featuresIcon4,
     desc: "Discover, save, and recall your favorite food items anytime, making every grocery run a walk in the park.",
